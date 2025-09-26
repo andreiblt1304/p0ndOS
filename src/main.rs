9568187da7eb1bc8a060a74965dcbec4c819b043
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-09-26 15:57:26 +0300

    Added custom test runner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+// have to rename the test runner generated function because it's called "main"
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -9,6 +13,9 @@ mod vga_buffer;
 pub extern "C" fn _start() -> ! {
     println!("HELLO from the p0nd OS!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -16,4 +23,22 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn test_assertion() {
+    print!("some test assertion...");
+    let a = true;
+    let b = true;
+    assert_eq!(a, b);
+    println!("[OK]");
 }
