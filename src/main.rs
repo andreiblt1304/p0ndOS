21df944ed15cb4e103df24be2f1cefa38f4a1c45
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-10-18 17:26:52 +0300

    Use translation function from x86 crate

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,20 @@
 
 use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
-use p0nd_os::println;
+use p0nd_os::{memory, println};
+use x86_64::structures::paging::Translate;
 
 // type-checked way to define the function as the kernel entry point
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use p0nd_os::memory::translate_address_wrapper;
     use x86_64::VirtAddr;
 
     println!("HELLO from the p0nd OS!");
     p0nd_os::init();
 
     let physical_memory_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(physical_memory_offset) };
     let addresses = [
         // the identity-mapped vga buffer page
         0xb8000,
@@ -31,8 +32,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     ];
     for &address in &addresses {
         let virtual_address = VirtAddr::new(address);
-        let physical_address =
-            unsafe { translate_address_wrapper(virtual_address, physical_memory_offset) };
+        let physical_address = mapper.translate_addr(virtual_address);
 
         println!("{:?} -> {:?}", virtual_address, physical_address);
     }
--- src/memory.rs
+++ src/memory.rs
@@ -1,15 +1,7 @@
-use x86_64::{PhysAddr, VirtAddr, structures::paging::PageTable};
-
-pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
-    use x86_64::registers::control::Cr3;
-
-    let (level_4_table_frame, _) = Cr3::read();
-    let physical_address = level_4_table_frame.start_address();
-    let virtual_address = physical_memory_offset + physical_address.as_u64();
-    let page_table_ptr: *mut PageTable = virtual_address.as_mut_ptr();
-
-    unsafe { &mut *page_table_ptr }
-}
+use x86_64::{
+    PhysAddr, VirtAddr,
+    structures::paging::{OffsetPageTable, PageTable},
+};
 
 // translate virtual address to the corresponding physical one if exists
 // it is unsafe because the caller guarantees that the physical memory is mapped to a virtual one
@@ -49,4 +41,24 @@ fn translate_address(address: VirtAddr, physical_memory_offset: VirtAddr) -> Opt
     }
 
     Some(frame.start_address() + u64::from(address.page_offset()))
+}
+
+// unsafe because the caller must guarantee that
+// the complete physical memory is mapped to virtual one at the passed offset
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    unsafe {
+        let level_4_table = active_level_4_table(physical_memory_offset);
+        OffsetPageTable::new(level_4_table, physical_memory_offset)
+    }
+}
+
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_table_frame, _) = Cr3::read();
+    let physical_address = level_4_table_frame.start_address();
+    let virtual_address = physical_memory_offset + physical_address.as_u64();
+    let page_table_ptr: *mut PageTable = virtual_address.as_mut_ptr();
+
+    unsafe { &mut *page_table_ptr }
 }
