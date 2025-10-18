529cd25415b316eb02a73d6be906603fa751178d
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-10-18 17:11:14 +0300

    Added function to translate virtual address to physical one

--- src/memory.rs
+++ src/memory.rs
@@ -1,4 +1,4 @@
-use x86_64::{VirtAddr, structures::paging::PageTable};
+use x86_64::{PhysAddr, VirtAddr, structures::paging::PageTable};
 
 pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
     use x86_64::registers::control::Cr3;
@@ -9,4 +9,44 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static
     let page_table_ptr: *mut PageTable = virtual_address.as_mut_ptr();
 
     unsafe { &mut *page_table_ptr }
+}
+
+// translate virtual address to the corresponding physical one if exists
+// it is unsafe because the caller guarantees that the physical memory is mapped to a virtual one
+pub unsafe fn translate_address_wrapper(
+    address: VirtAddr,
+    physical_memory_offset: VirtAddr,
+) -> Option<PhysAddr> {
+    translate_address(address, physical_memory_offset)
+}
+
+// safe function to limit the scope of unsafe
+// the wrapper can only be called using unsafe blocks outside the module
+fn translate_address(address: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    use x86_64::registers::control::Cr3;
+    use x86_64::structures::paging::page_table::FrameError;
+
+    let (level_4_table_frame, _) = Cr3::read();
+    let table_indexes = [
+        address.p4_index(),
+        address.p3_index(),
+        address.p2_index(),
+        address.p1_index(),
+    ];
+    let mut frame = level_4_table_frame;
+
+    for &index in &table_indexes {
+        let virtual_address = physical_memory_offset + frame.start_address().as_u64();
+        let table_ptr: *const PageTable = virtual_address.as_ptr();
+        let table = unsafe { &*table_ptr };
+
+        let entry = &table[index];
+        frame = match entry.frame() {
+            Ok(frame) => frame,
+            Err(FrameError::FrameNotPresent) => return None,
+            Err(FrameError::HugeFrame) => panic!("huge pages not supported"),
+        }
+    }
+
+    Some(frame.start_address() + u64::from(address.page_offset()))
 }
