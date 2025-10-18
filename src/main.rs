6d3c1f3b02d3bdc71bff29254fe40c85db461df2
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-10-18 16:55:02 +0300

    Traversed level 3 table

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,21 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let l4_table = unsafe { active_level_4_table(physical_memory_offset) };
 
     for (i, entry) in l4_table.iter().enumerate() {
+        use x86_64::structures::paging::PageTable;
+
         if !entry.is_unused() {
             println!("L4 entry {}: {:?}", i, entry);
+
+            let physical_address = entry.frame().unwrap().start_address();
+            let virtual_address = boot_info.physical_memory_offset + physical_address.as_u64();
+            let ptr = VirtAddr::new(virtual_address).as_mut_ptr();
+            let l3_table: &PageTable = unsafe { &*ptr };
+
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!("  L3 Entry {}: {:?}", i, entry);
+                }
+            }
         }
     }
 
