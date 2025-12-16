f9ca2b386ff9dd184edaf6c294fb0f6c633fdf6a
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-12-16 17:42:54 +0200

    Added ScancodeStream

--- src/task/keyboard.rs
+++ src/task/keyboard.rs
@@ -5,6 +5,20 @@ use crate::println;
 
 static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
 
+pub struct ScancodeStream {
+    _private: (), // prevents construction of this struct from outside of the module
+}
+
+impl ScancodeStream {
+    pub fn new() -> Self {
+        SCANCODE_QUEUE
+            .try_init_once(|| ArrayQueue::new(100))
+            .expect("The `new` method should be called once");
+
+        ScancodeStream { _private: () }
+    }
+}
+
 pub(crate) fn add_scancode(scancode: u8) {
     if let Ok(queue) = SCANCODE_QUEUE.try_get() {
         if queue.push(scancode).is_err() {
