cbe4682a4b9803f50cab571ebd2e01d36bbaccef
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-12-16 14:57:40 +0200

    Added rummy raw and normal waker

--- src/task/simple_executor.rs
+++ src/task/simple_executor.rs
@@ -1,3 +1,8 @@
+use core::{
+    ptr::null,
+    task::{RawWaker, RawWakerVTable, Waker},
+};
+
 use alloc::collections::vec_deque::VecDeque;
 
 use crate::task::task::Task;
@@ -16,4 +21,19 @@ impl SimpleExecutor {
     pub fn spawn(&mut self, task: Task) {
         self.task_queue.push_back(task);
     }
+}
+
+fn dummy_raw_waker() -> RawWaker {
+    fn no_op(_: *const ()) {}
+    fn clone(_: *const ()) -> RawWaker {
+        dummy_raw_waker()
+    }
+
+    let vtable = &RawWakerVTable::new(clone, no_op, no_op, no_op);
+
+    RawWaker::new(null::<()>(), vtable)
+}
+
+fn dummy_waker() -> Waker {
+    unsafe { Waker::from_raw(dummy_raw_waker()) }
 }
