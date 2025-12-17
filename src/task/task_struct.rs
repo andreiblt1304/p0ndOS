2999e4bf7b54fa09bdd00886b3b945570f4bf992
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-12-17 14:45:28 +0200

    Added atomic task id

--- src/task/task_struct.rs
+++ src/task/task_struct.rs
@@ -1,17 +1,30 @@
 use alloc::boxed::Box;
 use core::{
     pin::Pin,
+    sync::atomic::{AtomicU64, Ordering},
     task::{Context, Poll},
 };
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct TaskId(u64);
+
+impl TaskId {
+    fn new() -> Self {
+        static NEXT_ID: AtomicU64 = AtomicU64::new(0);
+        TaskId(NEXT_ID.fetch_add(1, Ordering::Relaxed))
+    }
+}
+
 // can store different types of Futures with the `dyn` keyword == dynamically dispatched methods
 pub struct Task {
+    id: TaskId,
     future: Pin<Box<dyn Future<Output = ()>>>,
 }
 
 impl Task {
     pub fn new(future: impl Future<Output = ()> + 'static) -> Task {
         Task {
+            id: TaskId::new(),
             future: Box::pin(future),
         }
     }
